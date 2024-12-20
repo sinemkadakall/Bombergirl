813f935c371585a3020b17807106541b1dcccb62
sinemsooplay <96748658+sinemkadakall@users.noreply.github.com>
2024-12-20 13:20:54 +0300

    Animations made.

--- Bombergirl/Assets/Scripts/MovementController.cs
+++ Bombergirl/Assets/Scripts/MovementController.cs
@@ -15,9 +15,20 @@ public class MovementController : MonoBehaviour
     public KeyCode inputLeft = KeyCode.A;
     public KeyCode inputRight = KeyCode.D;
 
+
+    //Karakterin farklý yönlerdeki animasyonlarýný kontrol etmek için
+    public AnimatedSpriteRenderer spriteRendererUp;
+    public AnimatedSpriteRenderer spriteRendererDown;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+
+    //Þu anda aktif olan animasyonu saklar.
+    private AnimatedSpriteRenderer activeSpriteRenderer;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        activeSpriteRenderer = spriteRendererDown;
 
     }
 
@@ -25,26 +36,26 @@ public class MovementController : MonoBehaviour
     {
         if(Input.GetKey(inputUp))
         {
-            SetDirection(Vector2.up);
+            SetDirection(Vector2.up,spriteRendererUp);
         }
         else if(Input.GetKey(inputDown))
         {
-            SetDirection(Vector2.down);
+            SetDirection(Vector2.down, spriteRendererDown);
 
         }
         else if(Input.GetKey(inputLeft))
         {
-            SetDirection(Vector2.left);
+            SetDirection(Vector2.left,spriteRendererLeft);
 
         }
         else if (Input.GetKey(inputRight))
         {
-            SetDirection(Vector2.right);
+            SetDirection(Vector2.right,spriteRendererRight);
 
         }
         else
         {
-            SetDirection(Vector2.zero);
+            SetDirection(Vector2.zero,activeSpriteRenderer);
 
         }
       
@@ -58,11 +69,17 @@ public class MovementController : MonoBehaviour
         rb.MovePosition(position + translation);
     }
 
-    private void SetDirection(Vector2 newDirection)
+    private void SetDirection(Vector2 newDirection,AnimatedSpriteRenderer spriteRenderer)
     {
         direction = newDirection;
 
+        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
 
+        activeSpriteRenderer = spriteRenderer;
+        activeSpriteRenderer.idle = direction == Vector2.zero;
     }
 
 
